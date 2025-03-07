97568228c5de3c682083d925b89376223ebf8a89
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2025-03-07 11:27:45 +0800

    feat: create parser for training

---
+++ py-HW1-Image-Classification/utils/parser.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--lr", default=1e-5, type=float)
+    parser.add_argument("--device", default="cpu", type=str)
+    parser.add_argument("--stale", default=20, type=int)
+    parser.add_argument("--epochs", default=100, type=int)
+    parser.add_argument("--loss_function", default="CrossEntropy", type=str)
+    parser.add_argument("--optimizer", default="Adam", type=str)
+    parser.add_argument("--model", default="resnet18", type=str)
+    parser.add_argument("--model_save_path", default="./weights", type=str)
+    parser.add_argument("--model_save_prefix_name", default=None, type=str)
+    parser.add_argument("--model_save_interval", default=10, type=int)
+    parser.add_argument("--data_path", default="./data", type=str)
+    parser.add_argument("--train_data_name", default=None, type=str)
+    parser.add_argument("--valid_data_name", default=None, type=str)
+    parser.add_argument("--test_data_name", default=None, type=str)
+    return parser
