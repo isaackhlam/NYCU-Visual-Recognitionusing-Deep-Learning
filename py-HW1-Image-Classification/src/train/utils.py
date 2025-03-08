1581ba4eb2cc6fe2e54b0c78dbbf1bb98b1a17ed
isaackhlam <isaac.lamkh@gmail.com>
2025-03-08 23:26:15 +0800

    feat: build optimizer and loss function

---
+++ py-HW1-Image-Classification/src/train/utils.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.optim as optim
+
+def build_criterion(args, logger):
+    criterion = None
+    if args.loss_function == "CrossEntropy":
+        criterion = nn.CrossEntropyLoss()
+    else:
+        logger.error(f"Unknown Loss Function: {args.loss_function}")
+        raise Exception("Unknown Loss Function")
+
+    logger.info(f"Using Loss Function: {args.loss_function}")
+    return criterion
+
+def build_optimizer(args, logger, model):
+    optimizer = None
+    if args.loss_function == "Adam":
+        optimizer = optim.Adam(model.parameters, args.lr)
+    else:
+        logger.error(f"Unknown Optimizer: {args.optimizer}")
+        raise Exception("Unknown Optmizer")
+
+    logger.info(f"Using Optimizer: {args.optimizer}")
+    return optimizer
+
