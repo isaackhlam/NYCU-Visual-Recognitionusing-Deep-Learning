db86958da55dd81c501620c2cbaccaadd103930f
isaackhlam <isaac.lamkh@gmail.com>
2025-04-28 15:58:08 +0800

    perf: (hw3) optimize memory usage when evaluate

--- py-HW3-Instance-Segmentation/src/train/train.py
+++ py-HW3-Instance-Segmentation/src/train/train.py
@@ -3,46 +3,50 @@ import wandb
 from torchvision.ops import box_iou
 from tqdm import tqdm
 
-
 def evaluate(args, model, dataloader, epoch):
     model.eval()
-    all_preds = []
-    all_labels = []
-    p_bar = tqdm(dataloader, desc=f"Epoch {epoch+1}/{args.epochs}")
+    total_precision = 0.0
+    total_samples = 0
 
+    p_bar = tqdm(dataloader, desc=f"Epoch {epoch+1}/{args.epochs}")
+    iou_threshold = 0.5
     with torch.no_grad():
         for images, targets in p_bar:
+
             images = [image.to(args.device) for image in images]
             targets = [{k: v.to(args.device) for k, v in t.items()} for t in targets]
 
-            preds = model(images)
-            for pred, target in zip(preds, targets):
-                all_preds.append(pred)
-                all_labels.append(target)
+            predictions = model(images)
 
-    iou_threshold = 0.5
+            for pred, target in zip(predictions, targets):
+                gt_boxes = target["boxes"]
+                gt_labels = target["labels"]
+                pred_boxes = pred["boxes"]
+                pred_scores = pred["scores"]
+                pred_labels = pred["labels"]
 
-    aps = []
-    for gt, pred in zip(all_labels, all_preds):
-        gt_boxes = gt["boxes"]
-        gt_labels = gt["labels"]
-        pred_boxes = pred["boxes"]
-        pred_scores = pred["scores"]
-        pred_labels = pred["labels"]
+                if len(pred_boxes) == 0:
+                    continue
+                if len(gt_boxes) == 0:
+                    continue
 
-        iou = box_iou(pred_boxes, gt_boxes)
-        matches = iou > iou_threshold
+                iou = box_iou(pred_boxes, gt_boxes)
+                matches = iou > iou_threshold
 
-        TP = (matches.sum(dim=1) > 0).sum().item()
-        FP = (matches.sum(dim=1) == 0).sum().item()
-        FN = (matches.sum(dim=0) == 0).sum().item()
-        precision = TP / (TP + FP) if (TP + FP) > 0 else 0
-        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+                TP = (matches.sum(dim=1) > 0).sum().item()
+                FP = (matches.sum(dim=1) == 0).sum().item()
+                # FN = (matches.sum(dim=0) == 0).sum().item()
+                precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+                total_precision += precision
+                total_samples += 1
 
-        aps.append(precision)
-
-    mAP = sum(aps) / len(aps) if aps else 0
+                del gt_boxes, gt_labels, pred_boxes, pred_scores, pred_labels, iou, matches
+            del images, targets, predictions
+            torch.cuda.empty_cache()
+    mAP = total_precision / total_samples if total_samples > 0 else 0
     return mAP
+
+
 
 
 def save_model(args, epoch, model, optimizer, save_path):
