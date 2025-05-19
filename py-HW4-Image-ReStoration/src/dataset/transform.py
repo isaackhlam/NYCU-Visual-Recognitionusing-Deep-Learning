36fda0ecd574f3873b1aac7de0970da49a37c8b7
isaackhlam <isaac.lamkh@gmail.com>
2025-05-20 00:27:54 +0800

    feat: (hw4) strengthen augmentation

--- py-HW4-Image-ReStoration/src/dataset/transform.py
+++ py-HW4-Image-ReStoration/src/dataset/transform.py
@@ -5,6 +5,10 @@ from albumentations.pytorch import ToTensorV2
 
 def get_basic_transform():
     return A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.ColorJitter(
+            brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.5
+        ),
         A.Normalize(
             mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
         ),  # ImageNet
