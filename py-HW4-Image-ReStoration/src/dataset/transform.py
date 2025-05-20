a27737a57c31e10bc4bc24127333341ee6691ec2
isaackhlam <isaac.lamkh@gmail.com>
2025-05-21 01:43:41 +0800

    feat: (hw4) more strong augmentation

--- py-HW4-Image-ReStoration/src/dataset/transform.py
+++ py-HW4-Image-ReStoration/src/dataset/transform.py
@@ -6,8 +6,10 @@ def get_basic_transform(isTrain=True):
     if isTrain:
         return A.Compose(
             [
+                A.RandomResizedCrop(256, 256, scale=(0.8, 1.0)),
                 A.HorizontalFlip(p=0.5),
-                A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.5),
+                A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.7),
+                A.GaussNoise(var_limit=(5.0, 30.0), p=0.3),
                 A.Normalize(
                     mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
                 ),  # ImageNet
@@ -28,6 +30,14 @@ def get_basic_transform(isTrain=True):
 
 def get_degraded_transform():
     return A.OneOf([
-        A.RandomRain(brightness_coefficient=0.9, drop_width=1, blur_value=3, p=1.0),
-        A.RandomSnow(snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=2.0, p=1.0),
+        A.OneOf([
+            A.RandomRain(brightness_coefficient=0.9, drop_width=1, blur_value=3, p=1.0),
+            A.RandomRain(brightness_coefficient=0.8, drop_width=2, blur_value=2, p=1.0),
+            A.RandomRain(brightness_coefficient=0.7, drop_width=3, blur_value=1, p=1.0),
+        ], p=1.0),
+        A.OneOf([
+            A.RandomSnow(snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=2.0, p=1.0),
+            A.RandomSnow(snow_point_lower=0.2, snow_point_upper=0.4, brightness_coeff=1.8, p=1.0),
+            A.RandomSnow(snow_point_lower=0.3, snow_point_upper=0.5, brightness_coeff=1.5, p=1.0),
+        ], p=1.0),
     ], p=1.0)
