da45b8adc0d45f529fe2ce2482761943149c37db
isaackhlam <isaac.lamkh@gmail.com>
2025-05-22 03:19:43 +0800

    fix: (hw4) wrong augment argument

--- py-HW4-Image-ReStoration/src/dataset/transform.py
+++ py-HW4-Image-ReStoration/src/dataset/transform.py
@@ -8,7 +8,7 @@ def get_basic_transform(isTrain=True):
             [
                 A.HorizontalFlip(p=0.5),
                 A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.7),
-                A.GaussNoise(var_limit=(5.0, 30.0), p=0.3),
+                # A.GaussNoise(std_range=(5.0, 30.0), p=0.3),
                 A.Normalize(
                     mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
                 ),  # ImageNet
@@ -35,8 +35,8 @@ def get_degraded_transform():
             A.RandomRain(brightness_coefficient=0.7, drop_width=3, blur_value=1, p=1.0),
         ], p=1.0),
         A.OneOf([
-            A.RandomSnow(snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=2.0, p=1.0),
-            A.RandomSnow(snow_point_lower=0.2, snow_point_upper=0.4, brightness_coeff=1.8, p=1.0),
-            A.RandomSnow(snow_point_lower=0.3, snow_point_upper=0.5, brightness_coeff=1.5, p=1.0),
+            A.RandomSnow(method='texture', snow_point_range=[0.1, 0.3], brightness_coeff=2.0, p=1.0),
+            A.RandomSnow(method='texture', snow_point_range=[0.2, 0.4], brightness_coeff=1.8, p=1.0),
+            A.RandomSnow(method='texture', snow_point_range=[0.3, 0.5], brightness_coeff=1.5, p=1.0),
         ], p=1.0),
     ], p=1.0)
